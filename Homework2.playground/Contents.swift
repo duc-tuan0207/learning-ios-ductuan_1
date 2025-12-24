9d3f169b33d7f1a975a5e8571aed4a2540fc9875
TuanNguyen <ductuan02072005@gmail.com>
2025-12-24 14:53:41 +0700

    no message

--- Homework2.playground/Contents.swift
+++ Homework2.playground/Contents.swift
@@ -15,9 +15,8 @@ func validateSKU(_ sku: String?){
         }
         print("SKU hợp lệ: \(sku)")
     }
-print(validateSKU("SKU-123"))
+print(validateSKU("SKU-3636"))
 //bai9
-import UIKit
 func validatePrice(_ text: String?){
     guard let text = text,!text.isEmpty else {
         print("Chưa nhập giá")
@@ -33,5 +32,58 @@ func validatePrice(_ text: String?){
     }
     print("Giá hợp lệ: \(text)VND")
 }
-print(validatePrice("100.000"))
+print(validatePrice("36.000"))
 //bai10
+func calcTotal(priceText: String?, quantityText: String?){
+    guard let priceText, let quantityText else {
+        return
+    }
+    guard let price = Double(priceText), let quantity = Int(quantityText) else {
+        return
+    }
+    guard quantity >= 0 else {
+        print("Số lượng không hợp lệ")
+        return
+    }
+    let total = price * Double(quantity)
+    print("Thành Tiền:\(total)")
+}
+print(calcTotal(priceText:"360000", quantityText:"36"))
+//bai11
+struct Product {
+ let sku: String
+ let name: String
+ let price: Double
+}
+func createProduct(
+ sku: String?,
+ name: String?,
+ priceText: String?
+) -> Product? {
+    guard let sku = sku, !sku.isEmpty else {
+        print("SKU rỗng")
+        return nil
+    }
+    guard let name=name,!name.isEmpty else {
+        print("Chưa nhập tên")
+        return nil
+    }
+    guard let priceText = priceText, !priceText.isEmpty else {
+        print("Vui lòng nhập giá")
+        return nil
+    }
+    guard sku.hasPrefix("SKU-") else {
+        print("Sai định dạng")
+        return nil
+    }
+    guard let price = Double(priceText) else {
+        print("không hợp lệ")
+        return nil
+    }
+    guard price > 0 else {
+        print("Sai giá")
+        return nil
+    }
+    return Product(sku: sku, name: name, price: Double(priceText)!)
+}
+print(createProduct(sku: "SKU-36", name: "Thanh Hóa", priceText: "36666666666")!)
